42100ce0e43782b7523d9c528a6cd749b4c45fda
ydrecords <turntable@yellowdogrecords.com>
2019-03-17 21:18:03 -0400

    changing names to a class attribute

--- python/submitted/robot_name.py
+++ python/submitted/robot_name.py
@@ -3,14 +3,19 @@ from string import digits
 from random import choices as choices
 
 class Robot(object):
+    """
+    A class for robots with unique names of the form 2 uppercase letters
+    followed by 3 digits.  Names will be unique among instances of the class.
+    """
+    names = set()
 
     def __init__(self):
         self.name = ''
-        self.names = set()
         self.reset()
         return
 
     def reset(self):
+        """ Assign a new, unique name to this instance of the Robot class. """
         while True:
             self.name = ''.join(choices(abc, k=2) + choices(digits,k=3))
             if self.name not in self.names:
