cbf604bed48e9a7b5171cad31b87c1f066ba8eee
roadfoodr <mpowers@alum.mit.edu>
2019-12-05 01:14:21 -0500

    Reimplement with entirely vectorized approach

--- python/submitted/largest_series_product.py
+++ python/submitted/largest_series_product.py
@@ -1,23 +1,24 @@
 import numpy as np
 
+def rolling_window(a, window):
+    # from https://rigtorp.se/2011/01/01/rolling-statistics-numpy.html
+    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
+    strides = a.strides + (a.strides[-1],)
+    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
+
 def largest_product(series, size):
     """
     Given a string of digits, calculate the largest product for any contiguous
     substring of digits of length n.
     """
-    series_length = len(series)
-    if size > series_length:
+    if size > len(series):
         raise ValueError("Size must not be larger than series length.")
     if size < 0:
         raise ValueError("Size must not be less than 0.")
 
-    series_list = [int(c) for c in series if c.isdigit()]
-    if len(series_list) < series_length:
+    try:
+        series_array = np.fromiter(series, dtype='int64')
+    except ValueError:
         raise ValueError("All characters in series must be digits.")
-
-    series_slices = np.array([
-            series_list[i:i+size] 
-            for i in range(series_length - size + 1)
-            ])
     
-    return np.max(np.prod(series_slices, axis=1))
+    return np.max(np.prod(rolling_window(series_array, size), axis=1))
