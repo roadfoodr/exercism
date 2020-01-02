e26cdcd87bcc5662e2f792b45c29d5e00bbd6d56
roadfoodr <mpowers@alum.mit.edu>
2020-01-02 03:17:51 -0500

    luhn.py

---
+++ python/submitted/luhn.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class Luhn:
+    '''
+    Given a string, provide a method to determine whether it represents a 
+    number that is valid per the Luhn formula.  Strings containing characters 
+    other than digits and whitespace are invalid.
+    '''
+    def __init__(self, card_num):
+        self.card_num = card_num
+        
+    def valid(self):
+        card_chars = ''.join(self.card_num.split())
+        card_digits = [c for c in card_chars if c.isdigit()]
+        
+        if len(card_digits) < 2:
+            return False
+        elif len(card_digits) != len(card_chars):
+            return False
+        else:
+            card_digits = np.array(card_digits, dtype='int32')
+            # Step 1: double every second digit, starting from the right
+            card_digits[-2::-2] = card_digits[-2::-2] * 2
+            # Step 2: subtract 9 from any numbers that are now greater than 9
+            card_digits[card_digits > 9] = card_digits[card_digits > 9] - 9
+            return bool((np.sum(card_digits) % 10) == 0)
